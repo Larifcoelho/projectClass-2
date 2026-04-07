1408ec05a96f2abc95b896608f01289542ca5871
Larifcoelho <larissafcoelho9@gmail.com>
2026-04-07 16:38:57 -0300

    uc sistemas distribuídos e mobile

--- projectClass/src/main/java/com/example/projectClass/EstacaoController.java
+++ projectClass/src/main/java/com/example/projectClass/EstacaoController.java
@@ -1,6 +1,7 @@
 package com.example.projectClass;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,4 +11,18 @@ public class EstacaoController {
     public String responder(){
         return "Localização detectada: Setor Norte. Serviços: café e wifi disponíveis. ";
     }
+    @GetMapping("/status")
+    public String getStatus(){
+        return "Sistema Operacional detectado. Middleware Spring Boot em operação. ";
+    }
+    @GetMapping("/identificar")
+    public String identificar(@RequestParam String nome){
+        return "Olá, " + nome + " você está conectado a um nó autônomo do sistema! ";
+    }
+    @GetMapping("/watch")
+    public String getVideo(@RequestParam String v,
+                           @RequestParam String t){
+        return "Vídeo tocando: " + v + " no tempo " + t + "s";
+    }
+
 }
